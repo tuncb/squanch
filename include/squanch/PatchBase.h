570fb5f03e00b93f33e0693dc50e76e0ca36b53c
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2018-02-11 15:29:24 +0100

    S-9
    fixes #14
    fix compilation errors due to deleted member functions

--- include/squanch/PatchBase.h
+++ include/squanch/PatchBase.h
@@ -9,11 +9,6 @@ namespace squanch {
   class PatchBase {
   public:
     PatchBase(int id, PatchType pt) : m_type(pt), m_id(id) {}
-    PatchBase& operator=(PatchBase&& other)
-    {
-      this->m_id = other.m_id;
-      this->m_type = other.m_type;
-    }
 
     inline int         id() const { return m_id; }
     inline PatchType type() const { return m_type; }
